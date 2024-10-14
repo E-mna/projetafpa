454b7f91b2fe12381056cb798040322285bfafa0
lotfi3003 <51326208+lotfi3003@users.noreply.github.com>
2024-10-14 14:27:07 +0200

    feat:setup auth endpoints with doc

---
+++ authManager/src/main/java/com/company/DTO/UserDTO.java
@@ -0,0 +1,16 @@
+package com.company.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    public String login;
+    public String email;
+
+}
---
+++ authManager/src/main/java/com/company/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.company.repository;
+
+import com.company.entity.User;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserRepository implements PanacheRepository<User> {
+}
